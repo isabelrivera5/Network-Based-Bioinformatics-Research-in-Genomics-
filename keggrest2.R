2593f3b963cda84b0f329511d36040f75d57aa9a
isabel.rivera5 <isabel.rivera5@upr.edu>
2021-10-01 20:59:15 -0400

    Includes new changes with code to organize KEGGREST results

--- keggrest2.R
+++ keggrest2.R
@@ -1,7 +1,8 @@
-## The following section of this script will provide a list of Kegg Ids, which were original Refseq Ids
-## that were taken from a Blast output file and converted into Kegg Ids through the use of the KEGGREST Package
+## The following section of this script will provide a list of Kegg Ortholog Ids, which were original Refseq Ids
+## that were taken from a Blast output file and first converted into Kegg Ids through the use of the KEGGREST Package
 library(dplyr)
 library("KEGGREST")
+library(readr)
 
 ## Import output file given by Blast 
 blastoutputfile <- read.table("C:/Users/isabe/Downloads/Loki1-k1u.tsv")
@@ -37,15 +38,13 @@ tstlist6 <- list()
 # }
 
 
-
-
 ## This way is more direct, possibly faster but you will not know how fast it is executing #################
 
 ## Lets test with a small sub-sample to see how fast
-##tstlist4 <- as.data.frame(as.character(tstlist3[1:100,]))
-##tstlist6 <- keggConv("genes", unlist(tstlist4), querySize = 100)
+tstlist4 <- as.data.frame(as.character(tstlist3[1:100,]))
+tstlist6 <- keggConv("genes", unlist(tstlist4), querySize = 100)
 
-tstlist6 <- keggConv("genes", unlist(tstlist3), querySize = 100)
+#tstlist6 <- keggConv("genes", unlist(tstlist3), querySize = 100)
 
 
 ## We then take the resulting kegg ids from the previous function (depending on which method you used it will either be
@@ -59,3 +58,52 @@ for (i in unlist(tstlist6)) {
   listofkoids <- c(listofkoids, keggLink("ko", i[[1]]))
   
 }
+
+############ The following code serves to manipulate both output files taken from the previous KEGGREST ################
+ 
+############ processes in order to organize  the ids involved into a neat table ####################################
+
+## Refseq and KEGG ids file
+RS_KEGG <- read_delim("C:/Users/isabe/OneDrive/Documents/OceanicPicoalgaeMetagenomics-Package/Network-Based-Bioinformatics-Research-in-Genomics-/KOList6 FP.csv", ";", 
+                      escape_double = FALSE, trim_ws = TRUE)
+
+## Kegg and KO file
+KEGG_KO <- read_csv("ListofKoids - C.csv")
+
+## Kegg and their corresponding Refseq and KO ids
+RS_KO <- merge(RS_KEGG, KEGG_KO, by.x = "KEGG", by.y = "KEGG" , all.x = T)
+
+## Original BLAST output file assignment
+BL_RS <- originaloutputfile
+
+##Here we manipulate the results in order to get a table that re-duplicates the KO ids to get a sense of how many repeat ids there are
+BL_KO <- merge(BL_RS, RS_KO, by.x = "tstlist", by.y = "RefSeq", all.x = F)
+listofNA  <- is.na(BL_KO$KO)
+listofNA <- which(listofNA)
+## We will use this later ignore this for now
+BL_KOwithoutduplicates <- unique(BL_KOwithoutNA)
+## Contains duplicate KO ids! 200k approx
+BL_KOwithoutNA <- slice(BL_KO, -listofNA)
+
+###################### This part of the script compares the original blast output file with a new one  ####################
+###################### so that we can eliminate previously analyzed refseq id's and this way we  ###############
+###################### save more time when we minimize the file of the size  #########
+
+## Assign new blast output file
+newblastoutputfile <- read.table("C:/Users/isabe/Downloads/Loki3-u1-clean2.tsv")
+
+## We remove duplicates 
+newblastoutputfile <- unique(newblastoutputfile)
+originaloutputfile <- unique(as.data.frame(tstlist))
+
+## Verify which entries in the new file are in the original file  
+BOP2KOs <- merge(newblastoutputfile, BL_KOwithoutduplicates, by.x = "V1", by.y = "tstlist", all.x = F)
+List1and3 <-merge(newblastoutputfile, originaloutputfile, by.x = "V1", by.y = "tstlist", all.y = F)
+
+## Displays entries of newblastoutputfile that are not in originalblastoutputfile
+List1and3withNA <- as.data.frame(setdiff(newblastoutputfile$V1, List1and3$V1))
+
+## Search in keggrest!
+
+
+
